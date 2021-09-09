91988200bd6ddc122440e3015bf87c0095427125
Florin <mihaiflorin01194@yahoo.com>
2021-09-09 09:33:39 +0300

    Adding initial project setup

---
+++ SchoolOf.ShoppingCart/SchoolOf.Data.Abstraction/BaseEntityModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolOf.Data.Abstraction
+{
+    public abstract class BaseEntityModel
+    {
+        public long Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset CreatedAt{ get; set;}
+        public DateTimeOffset ModifiedAt { get; set; }
+    }
+}
