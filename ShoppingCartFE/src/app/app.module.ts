9c563abc40352bd8e6be2664efc3d317b3807480
Florin <mihaiflorin01194@yahoo.com>
2021-09-28 20:06:51 +0300

    Added the project to last setup

--- ShoppingCartFE/src/app/app.module.ts
+++ ShoppingCartFE/src/app/app.module.ts
@@ -1,3 +1,4 @@
+   
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,13 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ProductListComponent } from './products/product-list/product-list.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
